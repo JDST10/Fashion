eaefe352a2d6379b333c0df200e026e6ed5455f0
JDST10 <lienis94@gmail.com>
2024-07-01 12:27:52 +0200

    tested

--- source_db_init/init.sql
+++ source_db_init/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE Products (
     Brand_Prod_id SERIAL PRIMARY KEY,
     Brand VARCHAR(50),
     Descript VARCHAR(800),
-    Price FLOAT()
+    Price FLOAT
 );
 
 CREATE TABLE product_characteristics (
@@ -15,5 +15,5 @@ CREATE TABLE product_characteristics (
 CREATE TABLE product_img (
     id SERIAL PRIMARY KEY,
     Brand_id INTEGER REFERENCES Products(Brand_Prod_id),
-    image_name VARCHAR(50),
+    image_name VARCHAR(50)
 );
